527fe65a02fe69cc2a48d0faf277eb51b9074131
Amit Paswan <ak9425391@gmail.com>
2024-02-22 10:30:17 +0530

    initial commit

---
+++ src/components/DashBoard.jsx
@@ -0,0 +1,11 @@
+import MiniDrawer from "../ui/MiniDrawer";
+
+const DashBoard = () => {
+  return (
+    <div className="text-4xl text-center text-red-500">
+      <MiniDrawer />
+    </div>
+  );
+};
+
+export default DashBoard;
