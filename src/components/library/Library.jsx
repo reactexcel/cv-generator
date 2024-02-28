22bfce7bf0e15564bf9ed4a059bd3ae4e9832008
Amit Paswan <ak9425391@gmail.com>
2024-02-28 12:03:40 +0530

    library page ui fixed

--- src/components/library/Library.jsx
+++ src/components/library/Library.jsx
@@ -7,17 +7,17 @@ import { pdfjs } from "react-pdf";
 import Stack from "@mui/material/Stack";
 import ModalComponent from "./ModalComponent";
 import moment from "moment";
-import { Backdrop, Button, CircularProgress } from "@mui/material";
+import { Button } from "@mui/material";
 import Refereshing from "../Refereshing/Refereshing";
 import { useNavigate } from "react-router-dom";
-import EditIcon from '@mui/icons-material/Edit';
+import EditIcon from "@mui/icons-material/Edit";
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.js",
   import.meta.url
 ).toString();
 
 const Library = () => {
-  const navigate=useNavigate()
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const data = useSelector((state) => state.CvSlice.getCvData);
   const handleDownloadPdf = (link) => {
@@ -32,61 +32,60 @@ const Library = () => {
   };
 
   const [titleName, setTitleName] = useState(null);
-  const [loading, setloading] = useState(false)
+  const [loading, setloading] = useState(false);
   useEffect(() => {
     const getData = async () => {
       try {
-        setloading(true)
+        setloading(true);
 
         const response = await ApiFetching("GET", "user/get", null);
         const data = response.data;
         if (data.success === true) {
-          console.log(response.data);
           dispatch(setCvData(data.user));
           setTitleName(data.user.cvLink);
         }
       } catch (error) {
         console.log(error, "afsasd");
-      }finally{
-        setloading(false)
+      } finally {
+        setloading(false);
       }
     };
     getData();
   }, []);
 
-  if(loading){
-    return  <Refereshing/>
+  if (loading) {
+    return <Refereshing />;
   }
   return (
     <Stack
-      sx={{ wordBreak: "break-all",bgcolor:'#dfe4ea' }}
+      sx={{ wordBreak: "break-all", bgcolor: "#eeecf3" }}
       className="font-poppins  h-fit shadow-sm rounded-md flex flex-col">
       <Stack
         direction={{ md: "row", xs: "column" }}
         sx={{ justifyContent: "space-between", p: "30px" }}>
-        <div className="md:text-lg text-sm font-semibold">
+        <div className="md:text-lg text-sm  md:font-semibold font-medium">
           Name:{data && data.firstname + " " + data.lastname}
         </div>
-        <div className="md:text-lg text-sm font-semibold ">
+        <div className="md:text-lg text-sm  md:font-semibold font-medium ">
           {" "}
           Email:{data && data.email}
         </div>
-        <div className="ms:text-lg text-sm font-semibold">
+        <div className="ms:text-lg text-sm md:font-semibold font-medium">
           Mobile No :{data && data.mobile}
         </div>
       </Stack>
       <div>
-        <Stack className="border-2 p-4 text-lg rounded-sm gap-2">
+        <Stack className="p-4 text-lg rounded-sm gap-2">
           {data &&
             data.cvLink &&
             data.cvLink.map((link, index) => (
               <div
-                className="flex flex-col gap-1 w-full border-2 border-gray-400 p-2 rounded-md"
+                className="flex flex-col gap-1 w-full border-2 border-gray-300 p-2 rounded-md"
                 key={index}>
-                <div className="flex font-medium w-full  justify-between">
-                  <div className="flex gap-2 leading-0">
-                    <div> {index + 1}.</div>
-                    <div>
+                <div className="flex font-medium w-full justify-between">
+                  <div className="flex flex-col md:flex-row gap-2  justify-between w-full leading-0">
+                    <div className="flex mt-2 px-2">
+                      <div className="text-sm font-medium"> {index + 1}.</div>
                       <div className="text-sm">
                         {typeof link.link === "string" &&
                           link.link.replace(
@@ -94,20 +93,29 @@ const Library = () => {
                             ""
                           )}
                       </div>
-                      <div className="text-sm font-light opacity-95">
+                    </div>
+
+                    <div className="flex md:w-[40%] justify-between flex-col md:flex-row">
+                      <div className="text-xs mt-3 font-light opacity-95">
                         Last Updated:{" "}
-                        {moment(link.updatedAt).format("MMMM DD, YYYY hh:mm A")}
+                        {moment(link.updatedAt).format("DD, MMMM hh:mm A")}
+                      </div>
+                      <div className="h-fit max-w-sm flex gap-2">
+                        <ModalComponent link={link.link} />
+                        <DownloadIcon
+                          className="mt-2"
+                          onClick={() => handleDownloadPdf(link.link)}
+                        />
+                        <Button
+                          disabled={!link.templetId}
+                          onClick={() =>
+                            navigate(`../editCvGenerator/${link.templetId}`)
+                          }>
+                          <EditIcon />
+                        </Button>
                       </div>
                     </div>
                   </div>{" "}
-                  <div className="h-fit max-w-sm flex gap-2">
-                    <ModalComponent link={link.link} />
-                    <DownloadIcon
-                      className="mt-2"
-                      onClick={() => handleDownloadPdf(link.link)}
-                    />
-                    <Button disabled={!link.templetId} onClick={()=>navigate(`../editCvGenerator/${link.templetId}`)}><EditIcon/></Button>
-                  </div>
                 </div>
               </div>
             ))}
