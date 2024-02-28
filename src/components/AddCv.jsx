23163f1603a9b3f6feac17c0946cdccff3973c10
Amit Paswan <ak9425391@gmail.com>
2024-02-28 11:03:36 +0530

    preview img added

--- src/components/AddCv.jsx
+++ src/components/AddCv.jsx
@@ -2,10 +2,11 @@ import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import { Button, Stack } from "@mui/material";
 import { Box } from "@mui/system";
 import { useFormik } from "formik";
-import React, { useState } from "react";
+import { useState } from "react";
 import * as Yup from "yup";
 import { toast } from "react-toastify";
 import ApiFetching from "../services/ApiFetching";
+import { Document, Page } from "react-pdf";
 
 const AddCv = () => {
   const [isDragging, setIsDragging] = useState(false);
@@ -22,25 +23,39 @@ const AddCv = () => {
         const formData = new FormData();
         console.log(formData);
         formData.append("resume", values.file);
+        console.log(values.file);
         const response = await ApiFetching("POST", "/user/upload", formData);
         console.log("Upload response:", response);
-        toast.success('Your File has been Uploaded Successfully');
-        resetForm(); // Clear form data
+        toast.success("Your File has been Uploaded Successfully");
+        resetForm();
       } catch (error) {
         console.error("Upload error:", error);
       }
       setSubmitting(false);
     },
   });
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [pdfFile, setPdfFile] = useState(null);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
 
   return (
     <div>
-      <Stack sx={{ justifyContent: 'center', alignItems: "center", width: '100%' }}>
-        <div className="sm:max-w-lg m-auto p-10 bg-white rounded-xl z-10">
+      <Stack
+        sx={{ justifyContent: "center", alignItems: "center", width: "100%" }}>
+        <div className="w-full lg:w-[40%] m-auto md:p-10 md:w-[60%] p-2 bg-white rounded-xl z-10">
           <div className="text-center">
-            <h2 className="mt-5 text-3xl font-bold text-gray-900">Please Upload C.V</h2>
+            <h2 className="mt-5 md:text-2xl text-sm w-full font-poppins md:font-semibold font-medium text-gray-900">
+              Please Upload C.V
+            </h2>
           </div>
-          <form className="mt-8 space-y-3" onSubmit={formik.handleSubmit}>
+          <form
+            className="mt-8 w-full space-y-3"
+            onSubmit={formik.handleSubmit}>
             <div className="grid grid-cols-1 space-y-2">
               <label
                 htmlFor="file"
@@ -55,35 +70,54 @@ const AddCv = () => {
                   setIsDragging(false);
                   const file = e.dataTransfer.files[0];
                   formik.setFieldValue("file", file);
-                }}
-              >
+                }}>
                 Attach Document
               </label>
               <div
-                className={`flex items-center justify-center w-full ${isDragging ? 'border-blue-500 border-dashed' : 'border-gray-300 border'} rounded-lg h-60 p-10 group text-center`}
-              >
+                className={`flex items-center justify-center w-full ${
+                  isDragging
+                    ? "border-blue-500 border-dashed"
+                    : "border-gray-300 border"
+                } rounded-lg h-60 p-10 group text-center`}>
                 <label className="flex flex-col rounded-lg w-full h-full">
-                  <div className="h-full w-full text-center flex flex-col items-center justify-center">
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                      <CloudUploadIcon />
-                    </Box>
-                    <p className="pointer-none text-gray-500 ">
-                      <span className="text-sm">Drag and drop</span> files here <br /> or{" "}
-                      <span className="text-blue-600 hover:underline cursor-pointer">
-                        select a file
-                      </span>{" "}
-                      from your computer
-                    </p>
+                  <div className="h-full w-full text-center flex flex-col items-center  justify-center">
+                    {pdfFile ? (
+                      <div className="md:h-[200px] md:w-[150px] w-full h-fit overflow-hidden">
+                        <Document
+                          file={pdfFile}
+                          onLoadSuccess={onDocumentLoadSuccess}>
+                          <Page pageNumber={1} />
+                        </Document>
+                      </div>
+                    ) : (
+                      <>
+                        <Box sx={{ display: "flex", alignItems: "center" }}>
+                          <CloudUploadIcon />
+                        </Box>
+                        <p className="pointer-none text-gray-500 ">
+                          <span className="text-sm">Drag and drop</span> files
+                          here <br /> or{" "}
+                          <span className="text-blue-600 hover:underline cursor-pointer">
+                            select a file
+                          </span>{" "}
+                          from your computer
+                        </p>
+                        <input
+                          type="file"
+                          id="file"
+                          name="file"
+                          style={{ position: "absolute", left: "-9999px" }}
+                          onChange={(e) => {
+                            formik.setFieldValue(
+                              "file",
+                              e.target.files[0] || null
+                            );
+                            setPdfFile(e.target.files[0]);
+                          }}
+                        />
+                      </>
+                    )}
                   </div>
-                  <input
-                    type="file"
-                    id="file"
-                    name="file"
-                    className="hidden"
-                    onChange={(e) => {
-                      formik.setFieldValue("file", e.target.files[0] || null);
-                    }}
-                  />
                 </label>
               </div>
               {formik.errors.file && (
@@ -94,10 +128,7 @@ const AddCv = () => {
               <span>File type: doc, pdf, types of images</span>
             </p>
             <div>
-              <Button variant="contained"
-                fullWidth
-                type="submit"
-              >
+              <Button variant="contained" fullWidth type="submit">
                 Upload
               </Button>
             </div>
